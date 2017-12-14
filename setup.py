c3496c12fa9ec7a55249889519c1a02af0749fbb
GraySliver <568756361@qq.com>
2017-12-14 16:25:47 +0800

    update 0.1.4 version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='pandas-files',
-    version='0.1.3',
+    version='0.1.4',
     author='GraySilver',
     author_email='allenyzx@163.com',
     url='https://upload.pypi.org/allenyzx/',
